745c8522e885d244bbc02620bc88dde4d8177a34
JBritt308 <jamesbritt308@gmail.com>
2018-09-15 01:40:01 -0400

    Added Color and YMD Point Matrices

--- MMM.m
+++ MMM.m
@@ -12,18 +12,30 @@ Mz = xlsread('TireDatabase.xls',SheetMz);
 
 vehicle=lapsim.vehicle;
 i=0;
-for beta=-7:.5:7
+x = -7;
+y = 7;
+step =.5;
+for beta=x:step:y
     
-    for delta=-7:.5:7
+    for delta=x:step:y
         i=i+1;
         [ymdA(i),ymdN(i)] = MMMpoint(beta,delta,R,vehicle,Fy,Mz);
+        YMD(i,:) = [beta delta ymdA(i) ymdN(i)] ;
     end
 end
-scatter(ymdA,ymdN,10,'filled')
+s = size(YMD);
+d = step*(x+y) + 1;
+color = linspace(1,10,s(1)/d);
+colorMat = zeros(0);
+for t = 1:1:s(1)/d
+    colorMat = horzcat(colorMat,color(t)*ones(d));
+end
+scatter(ymdA,ymdN,10,colorMat,'filled')
 axis([-3 3 -5000 5000])
 grid on
 
 max(abs(ymdA))
 max(abs(ymdN))
+YMD(1,:)
 end
 
