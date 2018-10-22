8d5fa87a0c19ed8f1c46df54716c2f244d30b6ea
JBritt308 <jamesbritt308@gmail.com>
2018-10-22 16:53:57 -0400

    Updated Tire Models

--- MMM.m
+++ MMM.m
@@ -40,10 +40,13 @@ end
 scatter(ymdA,ymdN,10,colorMat,'filled')
 axis([-3 3 -1 1])
 grid on
+xlabel('Lateral Acceleration (g)');
+ylabel('Yaw Coefficient');
+title(['Milliken Moment Diagram for ',int2str(Vx),' mph']);
 
 for i=-7:1:7 %Calculate Stability&Control
 cindex=find(YMD(:,1)==i); %Form Control index for beta=i
-sindex=find(YMD(:,2)==i); %Form Syability index for delta=i
+sindex=find(YMD(:,2)==i); %Form Stability index for delta=i
 
 YMD(cindex(1),6)=YMD(cindex(2),4)-YMD(cindex(1),4);
 YMD(sindex(1),5)=YMD(sindex(2),4)-YMD(sindex(1),4);
@@ -85,7 +88,7 @@ for y=1:1:15
     yy = ppval(pp,xx);
     
     figure(2);
-    plot(xspline,yspline,xx,yy);
+    plot(xx,yy,'blue');
     L = findobj(2,'type','line');
     copyobj(L,findobj(1,'type','axes'));
     close(2);
@@ -100,11 +103,32 @@ for z=1:1:15
     yy = ppval(pp2,xx);
     
     figure(2);
-    plot(xspline,yspline,xx,yy);
+    plot(xx,yy,'red');
     L = findobj(2,'type','line');
     copyobj(L,findobj(1,'type','axes'));
     close(2);
 end
+
+figure(3);
+xspline = YMD(1+7*15:15+7*15,3);
+yspline = YMD(1+7*15:15+7*15,5);
+xx = linspace(min(xspline),max(xspline),100);
+pp2 = spline(xspline,yspline);
+yy = ppval(pp2,xx);
+plot(xx,yy);
+xlabel('Lateral Acceleration (g)');
+ylabel('Stability (dN/dD)');
+title('Stability thru Origin');
+
+figure(4);
+yspline = YMD(1+7*15:15+7*15,6);
+xx = linspace(min(xspline),max(xspline),100);
+pp2 = spline(xspline,yspline);
+yy = ppval(pp2,xx);
+plot(xx,yy);
+xlabel('Lateral Acceleration (g)');
+ylabel('Control (dN/dB)');
+title('Control thru Origin');
 end
 
 end
